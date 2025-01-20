f04a3d2ad20b61a5a64283eb23d72f89e5119595
Alex Chudíc <alexc@glaroam2-181-226.wireless.gla.ac.uk>
2025-01-20 21:22:19 +0000

    Create a script for running sonar-scanner

---
+++ tmp/evaluate-repository.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if a path parameter is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <path> <sonar_token>"
+    exit 1
+fi
+
+# Assign the provided parameters to a variable
+directory="$1"
+sonar_token="$2"
+
+# Change to the specified directory
+
+current_directory=$(pwd)
+cd "$directory" || exit 1
+
+sonar_scanner_output=$(sonar-scanner \
+  -Dsonar.projectKey=test-suite-enhancement \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.token=$sonar_token \
+  -Dsonar.java.binaries=.
+)
+
+analysis_id=$(echo "$sonar_scanner_output" | grep -o 'http://localhost:9000/api/ce/task?id=.*')
+
+echo "ANALYSIS_URL: $analysis_id"
+# echo "Sonar Scanner Output: $sonar_scanner_output"
