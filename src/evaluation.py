8dddbd542d85390739336aeeb2e2d9eb4561a254
Alex Chudíc <alexc@glaroam2-173-121.wireless.gla.ac.uk>
2025-02-27 12:46:12 +0000

    Add section of the pipeline which formats the result from sonarqube and stores it in a json

--- src/evaluation.py
+++ src/evaluation.py
@@ -6,8 +6,9 @@ import os
 import base64
 from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
+from datetime import datetime
 
-def evaluate_project_directory(project_name, directory_path=None):
+def evaluate_project_directory(project_name, identifiers={}, directory_path=None):
     """Evaluates a repository using the SonarQube API."""
     
     sonar_project_name = os.getenv("SONAR_PROJECT_NAME")
@@ -30,13 +31,19 @@ def evaluate_project_directory(project_name, directory_path=None):
         directory = ""
     sonar_qube_url = (f"http://localhost:9000/api/measures/component?component={sonar_project_name}{directory}" 
                       "&metricKeys=code_smells%2Cnew_code_smells%2Clines%2Cnew_lines%2Cncloc%2Cbugs%2Cnew_bugs%2C"
-                      "vulnerabilities%2Cnew_vulnerabilities%2Cnew_maintainability_rating%2Csqale_index%2Ccomplexity%2C"
-                      "cognitive_complexity%2Ccomment_lines")
+                      "vulnerabilities%2Cnew_vulnerabilities%2Cbranch_coverage%2Csqale_index%2Ccomplexity%2C"
+                      "cognitive_complexity%2Ccomment_lines%2Clines_to_cover%2Cuncovered_lines%2Ccoverage%2Cline_coverage")
     sonar_qube_result = make_get_request(sonar_qube_url)
     
+    identifiers["directory"] = directory_path
+    formated_results = format_sonarqube_results(sonar_qube_result, project_name, identifiers)
+
+    save_path = os.path.join("data", project_name, "eval", "sonarqube_results.json")
+    save_sonarqube_results(formated_results, save_path)
+
     # Print and return the result
-    print("SonarQube Result:\n" + sonar_qube_result)
-    return sonar_qube_result
+    print("SonarQube Result:\n" + formated_results)
+    return formated_results
 
 
 def execute_sonarqube_evaluation(project_name):
@@ -46,7 +53,6 @@ def execute_sonarqube_evaluation(project_name):
     if not sonar_token:
         raise ValueError("SONAR_TOKEN is not set in environment variables.")
 
-    print(sonar_token)
     # Command to execute
     command = ["/bin/bash", "src/bash-scripts/evaluate-repository.sh", "tmp/" + project_name + "/" , sonar_token]
 
@@ -146,6 +152,48 @@ def evaluate_test_suite(project_name):
         print("Error executing command.")
         print("Exit Code:", e.returncode)
         print("Error Output:\n", e.stderr)
+
+
+def format_sonarqube_results(results, project_name, identifiers):
+    """Formats the SonarQube results into a more readable format."""
+    
+    try:
+        data = json.loads(results)
+        metrics = {}
+        for measure in sorted(data["component"]["measures"], key=lambda m: m["metric"]):
+            if "value" in measure:
+                metrics[measure["metric"]] = measure["value"]
+            elif "period" in measure:
+                metrics[measure["metric"]] = measure["period"]["value"]
+        
+        output = {
+            "project" : project_name,
+            "identifiers": identifiers,
+            "metrics": metrics,
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        return json.dumps(output, indent=4)
+    except Exception as e:
+        print("Error formatting SonarQube results:", e)
+        return None
+
+
+def save_sonarqube_results(results, path):
+    """Saves the SonarQube results to a file."""
+    # Append to existing file if it exists
+    if not os.path.exists(path):
+        with open(path, "w") as file:
+            json.dump([], file, indent=4)
+
+    with open(path, "r+") as file:
+        existing_results = json.load(file)
+        existing_results.append(json.loads(results))
+        file.seek(0)
+        json.dump(existing_results, file, indent=4)
+
+    print("The sonarqube results have been saved to ", path)
+
+        
 
 
 if __name__ == "__main__":
